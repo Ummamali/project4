01c745c6261dc235fd4411e680a2f48122beaf16
UmmamComputer <sblue7446@gmail.com>
2020-09-26 02:18:59 +0500

    Added response to eux section

---
+++ script/main.js
@@ -0,0 +1,42 @@
+// This javascript file contains comments as follows
+
+
+// hover effect for master eux section in the about.html page
+document.addEventListener("DOMContentLoaded", function(){
+  let current = document.getElementById("main-card");
+  current.style.zIndex = "1";
+  current.style.opacity = "1";
+  document.getElementById("documentation").onmouseover = function (){
+      current.style.opacity = "0";
+      current.style.zIndex = "0";
+      current = document.getElementById("exp-documentation");
+      current.style.opacity = "1";
+      current.style.zIndex = "1"
+  };
+
+  document.getElementById("innovation").onmouseover = function (){
+      current.style.opacity = "0";
+      current.style.zIndex = "0";
+      current = document.getElementById("exp-innovation");
+      current.style.opacity = "1";
+      current.style.zIndex = "1"
+  };
+
+  document.getElementById("code").onmouseover = function (){
+      current.style.opacity = "0";
+      current.style.zIndex = "0";
+      current = document.getElementById("exp-code");
+      current.style.opacity = "1";
+      current.style.zIndex = "1"
+  };
+
+  document.getElementById("responsive").onmouseover = function (){
+      current.style.opacity = "0";
+      current.style.zIndex = "0";
+      current = document.getElementById("exp-responsive");
+      current.style.opacity = "1";
+      current.style.zIndex = "1"
+  };
+
+   
+});
