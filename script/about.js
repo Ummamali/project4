00333136a5139c74bf5a15b90bd5150ed3b5d854
UmmamComputer <sblue7446@gmail.com>
2020-09-27 01:10:10 +0500

    completed about page

--- script/about.js
+++ script/about.js
@@ -3,6 +3,7 @@
 
 // hover effect for master eux section in the about.html page
 document.addEventListener("DOMContentLoaded", function(){
+    // The below code is for eux section hover effect 
   let current = document.getElementById("main-card");
   current.style.zIndex = "1";
   current.style.opacity = "1";
@@ -38,5 +39,42 @@ document.addEventListener("DOMContentLoaded", function(){
       current.style.zIndex = "1"
   };
 
-   
+    //   The code below is for the testimonials section
+  document.getElementById("markus").onmouseover = function (){
+    make_big();
+    this.style.transform = "scale(1.5)";
+    this.style.zIndex = "1";
+  };
+  document.getElementById("baby").onmouseover = function (){
+    make_big();
+    this.style.transform = "scale(1.5)";
+    this.style.zIndex = "1";
+  };
+  document.getElementById("joseph").onmouseover = function (){
+    make_big();
+    this.style.transform = "scale(1.5)";
+    this.style.zIndex = "1";
+  };
+  document.getElementById("ken").onmouseover = function (){
+    make_big();
+    this.style.transform = "scale(1.5)";
+    this.style.zIndex = "1";
+  };
+
+
+//   resets on mouse leave
+  document.getElementById("markus").onmouseleave = reset;
+  document.getElementById("baby").onmouseleave = reset;
+  document.getElementById("joseph").onmouseleave = reset;
+  document.getElementById("ken").onmouseleave = reset;  
 });
+
+
+function reset(){
+    this.style.transform = "scale(1)";
+    this .style.zIndex = "0";
+}
+
+function make_big (){
+    document.querySelector(".people").style.height = "300px";
+}
