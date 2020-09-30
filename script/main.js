eb1d6094610f4f17a7581ebfc49bde59ca526dc4
UmmamComputer <sblue7446@gmail.com>
2020-09-30 23:48:10 +0500

    same as above

---
+++ script/main.js
@@ -0,0 +1,20 @@
+// the following script is for global 
+
+document.addEventListener("DOMContentLoaded", function (){
+  let toggled = false;
+  document.getElementById("toggler").onclick = function (){
+    let res_nav = document.getElementById("res-nav");
+    if (toggled == false){
+      this.innerHTML = "<i class='fas fa-times'></i>";
+      this.style.transform = "rotate(360deg)";
+      res_nav.style.transform = "translateX(-100vw)";
+      toggled = true;
+    }else{
+      this.innerHTML = "<i class='fas fa-bars'></i>";
+      this.style.transform = "rotate(-360deg)";
+      res_nav.style.transform = "translateX(0vw)";
+      toggled = false;
+    }
+    
+  };
+});
